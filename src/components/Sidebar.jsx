e8bbba2292b8e8d604ce9f61fa272fd5b6adcb57
Diana <dianadorojuc3@gmail.com>
2025-11-03 23:35:46 +0700

    add sidebar for mobile + css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 import { Button, Col, Navbar, Nav, InputGroup, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import {
-  selectIsAuthenticated,
-} from "../redux/selectors/authSelectors";
+import { selectIsAuthenticated } from "../redux/selectors/authSelectors";
 import { logoutUser } from "../redux/action/auth";
+import { Offcanvas } from "react-bootstrap";
 
 const Sidebar = ({ handleSearch, handleHomeClick }) => {
   const [query, setQuery] = useState("");
@@ -15,11 +14,16 @@ const Sidebar = ({ handleSearch, handleHomeClick }) => {
 
   const isAuth = useSelector(selectIsAuthenticated);
 
+  //Mostra sidebar da mobile
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const toggleMobile = () => setMobileOpen((v) => !v);
+  const closeMobile = () => setMobileOpen(false);
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       handleSearch(query);
       setQuery("");
+      closeMobile();
     }
   };
 
@@ -31,10 +35,12 @@ const Sidebar = ({ handleSearch, handleHomeClick }) => {
       if (ok) {
         dispatch(logoutUser());
         navigate("/register", { replace: true });
+        closeMobile();
       }
       return;
     }
     navigate("/register");
+    closeMobile();
   };
 
   const goLogin = () => {
@@ -47,98 +53,123 @@ const Sidebar = ({ handleSearch, handleHomeClick }) => {
         navigate("/login", { replace: true });
         //{ replace: true } -> La pag. precedente viene rimossa dalla cronologia
         // se l’utente preme “← Indietro”, non torna alla pagina in cui era loggato.
+        closeMobile();
       }
       return;
     }
     navigate("/login");
+    closeMobile();
   };
 
   return (
-    <Col md={2}>
-      <Navbar
-        expand="md"
-        className="left-nav navbar navbar-expand-md fixed-left justify-content-between"
-        fixed="left"
-        id="sidebar"
+    <>
+      <Button
+        className="mobile-menu-toggle"
+        onClick={toggleMobile}
+        aria-label="Open/Close menu"
       >
-        <div className="d-flex flex-column">
-          <Link
-            to="/"
-            className="text-decoration-none"
-            onClick={handleHomeClick}
-          >
-            <Navbar.Brand>
-              <img
-                className="mt-2"
-                src="assets/logo/spotify.png"
-                alt="Spotify Logo"
-                width="131"
-                height="40"
-              />
-            </Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse id="navbar-nav">
-            <Nav className="flex-column">
-              <Link
-                to="/"
-                onClick={handleHomeClick}
-                className={`nav-link ${
-                  location.pathname === "/" ? "active" : ""
-                }`}
-              >
-                <i className="bi bi-house-door-fill"></i>&nbsp; Home
-              </Link>
-              <Link
-                to="/liked-songs"
-                className={`nav-link ${
-                  location.pathname === "/liked-songs" ? "active" : ""
-                }`}
-              >
-                <i className="bi bi-book-fill"></i>&nbsp; Your Library
-              </Link>
-              <InputGroup className="mt-3 search-input ms-1">
-                <Form.Control
-                  placeholder="Search"
-                  aria-label="Search"
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-                  onKeyUp={handleKeyPress}
-                />
-                <Button
-                className="go-btn"
-                  variant="outline-secondary"
-                  onClick={() => {
-                    handleSearch(query);
-                    setQuery("");
-                  }}
-                >
-                  GO
-                </Button>
-              </InputGroup>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
+        <i className="bi bi-list" />
+      </Button>
 
-        <div className="mt-auto nav-btn">
-          <Button className="mb-2 btn signup-btn" onClick={goRegister}>
-            Sign Up
-          </Button>
-          <Button className="btn login-btn" onClick={goLogin}>
-            Login
-          </Button>
-          <div>
-            <a href="https://support.spotify.com/it/category/safety-privacy/">
-              Cookie Policy
-            </a>{" "}
-            |{" "}
-            <a href="https://support.spotify.com/it/category/safety-privacy/">
-              Privacy
-            </a>
+      <Col md={2}>
+        <Navbar
+          expand="md"
+          className={`left-nav navbar navbar-expand-md fixed-left justify-content-between ${
+            mobileOpen ? "open" : ""
+          }`}
+          fixed="left"
+          id="sidebar"
+        >
+          <div className="sidebar-inner d-flex flex-column">
+            <Link
+              to="/"
+              className="text-decoration-none"
+              onClick={() => {
+                handleHomeClick();
+                closeMobile();
+              }}
+            >
+              <Navbar.Brand>
+                <img
+                  className="mt-2"
+                  src="assets/logo/spotify.png"
+                  alt="Spotify Logo"
+                  width="131"
+                  height="40"
+                />
+              </Navbar.Brand>
+            </Link>
+          {/*   <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav"> */}
+              <Nav className="flex-column" id="navbar-nav">
+                <Link
+                  to="/"
+                  onClick={() => {
+                    handleHomeClick();
+                    closeMobile();
+                  }}
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                >
+                  <i className="bi bi-house-door-fill"></i>&nbsp; Home
+                </Link>
+                <Link
+                  to="/liked-songs"
+                  onClick={closeMobile}
+                  className={`nav-link ${
+                    location.pathname === "/liked-songs" ? "active" : ""
+                  }`}
+                >
+                  <i className="bi bi-book-fill"></i>&nbsp; Your Library
+                </Link>
+                <InputGroup className="mt-3 search-input ms-1">
+                  <Form.Control
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyUp={handleKeyPress}
+                  />
+                  <Button
+                    className="go-btn"
+                    variant="outline-secondary"
+                    onClick={() => {
+                      handleSearch(query);
+                      setQuery("");
+                      closeMobile();
+                    }}
+                  >
+                    GO
+                  </Button>
+                </InputGroup>
+              </Nav>
+        {/*    </Navbar.Collapse> */}
           </div>
-        </div>
-      </Navbar>
-    </Col>
+
+          <div className="mt-auto nav-btn">
+            <Button className="mb-2 btn signup-btn" onClick={goRegister}>
+              Sign Up
+            </Button>
+            <Button className="btn login-btn" onClick={goLogin}>
+              Login
+            </Button>
+            <div>
+              <a href="https://support.spotify.com/it/category/safety-privacy/">
+                Cookie Policy
+              </a>{" "}
+              |{" "}
+              <a href="https://support.spotify.com/it/category/safety-privacy/">
+                Privacy
+              </a>
+            </div>
+          </div>
+        </Navbar>
+        {mobileOpen && (
+          <div className="sidebar-overlay" onClick={closeMobile} />
+        )}
+      </Col>
+    </>
   );
 };
 
